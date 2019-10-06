7d9d338f59009587b87d5c891fc82ef6a0fc9b85
barasher <arnaud.barasher@gmail.com>
2019-10-06 23:01:10 +0200

    Does not export date and GPS attribute anymore (until format is fixed)

--- internal/model/model.go
+++ internal/model/model.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Model struct {
-	Aperture     float32   // Aperture
-	ShutterSpeed string    // ShutterSpeed
-	Keywords     []string  // Keywords
-	CameraModel  string    // Model
-	LensModel    string    // LensModel
-	MimeType     string    // MIMEType
-	Height       uint32    // ImageHeight
-	Width        uint32    // ImageWidth
-	FileSize     uint32    //
-	CaptureDate  time.Time // CaptureDate
-	GPS          string    // GPSPosition "41.12,-71.34"
+	Aperture     float32   
+	ShutterSpeed string    
+	Keywords     []string  
+	CameraModel  string    
+	LensModel    string    
+	MimeType     string    
+	Height       uint32    
+	Width        uint32    
+	FileSize     uint32    
+	CaptureDate  time.Time `json:"-"`
+	GPS          string    `json:"-"`
 }
