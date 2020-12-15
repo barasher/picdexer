d990a4ca5240b3d362907d3a8ac754f6d9d4e945
barasher <arnaud.barasher@gmail.com>
2020-12-16 00:03:23 +0100

    Elasticsearch refactoring (part 2)

--- internal/elasticsearch.go
+++ internal/elasticsearch.go
@@ -1,10 +1,24 @@
 package internal
 
-import "context"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+	"time"
+)
 
 const (
-
+	esDocIdentifier = "esDocId"
 	defaultBulkSize = 30
+	bulkSuffix      = "_bulk"
+	ndJsonMimeType  = "application/x-ndjson"
 )
 
 type EsDoc struct {
@@ -21,6 +35,7 @@ type EsPusher struct {
 
 type EsPusherConf struct {
 	BulkSize int    `json:"bulkSize"`
+	Url      string `json:"url"`
 }
 
 func NewEsPusher(conf EsPusherConf) (*EsPusher, error) {
@@ -36,7 +51,70 @@ func (p *EsPusher) bulkSize() int {
 	return n
 }
 
-func (*EsPusher) Push(ctx context.Context, inEsDocChan chan EsDoc) error {
+func (pusher *EsPusher) Push(ctx context.Context, inEsDocChan chan EsDoc) error {
+	buffer := bytes.Buffer{}
+	jsonEncoder := json.NewEncoder(&buffer)
+	bufferDocCount := 0
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case doc, ok := <-inEsDocChan:
+			if !ok {
+				if bufferDocCount > 0 {
+					if err := pusher.pushToEs(ctx, &buffer); err != nil {
+						return fmt.Errorf("error while pushing to elasticsearch: %w", err)
+					}
+				}
+				return nil
+			}
+			if err := jsonEncoder.Encode(doc.Header); err != nil {
+				log.Debug().Str(esDocIdentifier, doc.Header.ID).Msgf("Header: %v", doc.Header)
+				return fmt.Errorf("error while encoding header: %w", err)
+			}
+			if err := jsonEncoder.Encode(doc.Document); err != nil {
+				log.Debug().Str(esDocIdentifier, doc.Header.ID).Msgf("Body: %v", doc.Document)
+				return fmt.Errorf("error while encoding body: %w", err)
+			}
+			bufferDocCount++
+			if bufferDocCount == pusher.bulkSize() {
+				if err := pusher.pushToEs(ctx, &buffer); err != nil {
+					return fmt.Errorf("error while pushing to elasticsearch: %w", err)
+				}
+				bufferDocCount = 0
+			}
+		}
+	}
+
+	return nil
+}
+
+func (pusher *EsPusher) pushToEs(ctx context.Context, body io.Reader) error {
+	u, err := url.Parse(pusher.conf.Url)
+	if err != nil {
+		return fmt.Errorf("error while parsing elasticsearch url (%v): %w", pusher.conf.Url, err)
+	}
+	u.Path = path.Join(u.Path, bulkSuffix)
+
+	httpClient := &http.Client{
+		Timeout: 60 * time.Second,
+	}
+	resp, err := httpClient.Post(u.String(), ndJsonMimeType, body)
+	if err != nil {
+		return fmt.Errorf("error while pushing to Elasticsearch: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("error while reading response body: %w", err)
+		}
+		log.Error().Msgf("Response body: %v", string(b))
+		return fmt.Errorf("wrong status code (%v)", resp.StatusCode)
+	}
+
 	return nil
 }
 
--- internal/elasticsearch_test.go
+++ internal/elasticsearch_test.go
@@ -1,7 +1,12 @@
 package internal
 
 import (
+	"context"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -24,4 +29,115 @@ func TestBulkSize(t *testing.T) {
 			assert.Equal(t, tc.expValue, p.bulkSize())
 		})
 	}
+}
+
+func TestPushToEs_Nominal(t *testing.T) {
+	expBody := "sampleBody"
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "/_bulk", r.URL.Path)
+		assert.Equal(t, "application/x-ndjson", r.Header.Get("Content-type"))
+		b, err := ioutil.ReadAll(r.Body)
+		assert.Nil(t, err)
+		defer r.Body.Close()
+		assert.Equal(t, expBody, string(b))
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer ts.Close()
+
+	pusher, err := NewEsPusher(EsPusherConf{Url: ts.URL})
+	assert.Nil(t, err)
+	assert.Nil(t, pusher.pushToEs(context.TODO(), strings.NewReader(expBody)))
+}
+
+func TestPushToEs_BadHttpStatus(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer ts.Close()
+
+	pusher, err := NewEsPusher(EsPusherConf{Url: ts.URL})
+	assert.Nil(t, err)
+	assert.NotNil(t, pusher.pushToEs(context.TODO(), strings.NewReader("bla")))
+}
+
+func buildEsDoc(id string, filename string) EsDoc {
+	d := EsDoc{}
+	d.Header.Index = "idx"
+	d.Header.ID = id
+	d.Document = PictureMetadata{FileName: filename}
+	return d
+}
+
+func TestPush_Nominal(t *testing.T) {
+	collectedBodies := []string{}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "/_bulk", r.URL.Path)
+		assert.Equal(t, "application/x-ndjson", r.Header.Get("Content-type"))
+		b, err := ioutil.ReadAll(r.Body)
+		assert.Nil(t, err)
+		defer r.Body.Close()
+		collectedBodies = append(collectedBodies, string(b))
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer ts.Close()
+
+	pusher, err := NewEsPusher(EsPusherConf{Url: ts.URL, BulkSize: 2})
+	assert.Nil(t, err)
+
+	inChan := make(chan EsDoc, 4)
+	inChan <- buildEsDoc("id1", "f1.jpg")
+	inChan <- buildEsDoc("id2", "f2.jpg")
+	inChan <- buildEsDoc("id3", "f3.jpg")
+	close(inChan)
+
+	assert.Nil(t, pusher.Push(context.TODO(), inChan))
+	assert.Equal(t, 2, len(collectedBodies))
+	assert.Equal(t, "{\"_index\":\"idx\",\"_id\":\"id1\"}\n"+
+		"{\"FileName\":\"f1.jpg\",\"Folder\":\"\",\"ImportID\":\"\",\"FileSize\":0}\n"+
+		"{\"_index\":\"idx\",\"_id\":\"id2\"}\n"+
+		"{\"FileName\":\"f2.jpg\",\"Folder\":\"\",\"ImportID\":\"\",\"FileSize\":0}\n", collectedBodies[0])
+	assert.Equal(t, "{\"_index\":\"idx\",\"_id\":\"id3\"}\n"+
+		"{\"FileName\":\"f3.jpg\",\"Folder\":\"\",\"ImportID\":\"\",\"FileSize\":0}\n", collectedBodies[1])
+}
+
+func TestPush_ErrorOnPush(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer ts.Close()
+
+	pusher, err := NewEsPusher(EsPusherConf{Url: ts.URL, BulkSize: 2})
+	assert.Nil(t, err)
+
+	inChan := make(chan EsDoc, 1)
+	inChan <- buildEsDoc("id1", "f1.jpg")
+	close(inChan)
+
+	assert.NotNil(t, pusher.Push(context.TODO(), inChan))
+}
+
+func TestPush_ErrorOnPush2(t *testing.T) {
+	q := 0
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if q == 1 {
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		q++
+	}))
+	defer ts.Close()
+
+	pusher, err := NewEsPusher(EsPusherConf{Url: ts.URL, BulkSize: 2})
+	assert.Nil(t, err)
+
+	inChan := make(chan EsDoc, 3)
+	inChan <- buildEsDoc("id1", "f1.jpg")
+	inChan <- buildEsDoc("id2", "f2.jpg")
+	inChan <- buildEsDoc("id3", "f3.jpg")
+	close(inChan)
+
+	assert.NotNil(t, pusher.Push(context.TODO(), inChan))
 }
